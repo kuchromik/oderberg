6a77d264af0c27dbe35d610a8e3d98a012497901
Kai-Uwe Chromik <kai.chromik@online.de>
2024-05-23 07:56:02 +0200

    feat: Implement image search functionality in Textsearch component

--- src/lib/Textsearch.svelte
+++ src/lib/Textsearch.svelte
@@ -1,21 +1,42 @@
 <script>
     import { onMount } from 'svelte';
+    import { goto } from "$app/navigation";
+    import { doc, addDoc, deleteDoc, collection, onSnapshot, updateDoc, getDocs, query, where, getDoc } from "@firebase/firestore";
+    import { db } from "../firebase";
 
     let searchString = '';
+    let images = [];
 
-    // Function to search for the string in comments and answers
-    function search() {
-        // Logic to search for the string in comments and answers
-        // ...
+    const search = async () => {
+        const q = query(collection(db, "images"));
+        const querySnapshot = await getDocs(q);
+        images = [];
+        querySnapshot.forEach((doc) => {
+            if(doc.data().imagetitel.includes(searchString)) {
+                //images.push(doc);
+                images = [...images, { ...doc.data(), id: doc.id, url: doc.data().url }];
+            }
+        });
     }
 
-    // Trigger the search function when the component is mounted
-    onMount(search);
+  
 </script>
 <div class="searchinput">
-<input type="text" bind:value={searchString} placeholder="Nach Begriff suchen" />
+<input type="text" bind:value={searchString} placeholder="im Bildnamen ist enthalten ..." />
 
 <button on:click={search}>Suche starten</button>
+</div>
+
+<div class="imagedivision">
+    {#each images as image}
+        <div>
+            <a href="/locations/{image.location}/images/{image.id}">
+                <img src={image.url} alt={image.imagetitel} /></a>
+            <center>
+                <p>{image.imagetitel}</p>
+            </center>
+        </div>
+    {/each}
 </div>
 
 <style>
@@ -40,5 +61,26 @@
         border-radius: 5px;
         font-size: 1.2rem;
         background-color: #f0f0f0;
+    }
+
+    .resultlist {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+        grid-auto-rows: minmax(50px, auto);
+        gap: 10px;
+    }
+
+    .imagedivision {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    .imagedivision img {
+        width: 300px;
+        height: auto;
     }
 </style>
